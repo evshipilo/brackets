e7ff8677eb8bc423bf5475f76c1fd7f6f63f741a
Yauheni Shypila <evshipilo@gmail.com>
2020-03-03 11:58:23 +0300

    add: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    //если находим () то вырезаем, проверяем длину массива и начинаем сначала
+    // повторяем пока находим, если не находим то false
+    let strArr = str.split("");
+    let i;  //i глобальная чтобы сработало i=-1
+    for (i=0; i < strArr.length - 1; i++) {
+        for (let j=0; j < bracketsConfig.length; j++) {
+            if (strArr[i] == bracketsConfig[j][0] && strArr[i + 1] == bracketsConfig[j][1]) {
+                strArr.splice(i, 2);
+                if (strArr.length == 0) return true;
+                if (strArr.length == 1) return false;
+                i = -1; //global
+            }
+        }
+    }
+    return false;
 }
